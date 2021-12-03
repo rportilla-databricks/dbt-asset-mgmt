5f63028693bda6471ccfd01e1a0cdecba5732e6e
Ivan Trusov <prasad.kona@databricks.com>
2021-12-03 22:17:40 +0000

    fixxing

--- models/explore.sql
+++ models/explore.sql
@@ -1,7 +1,8 @@
 --select read_ts, ticker, avg(sentiment.compound) from {{ref('stg_sentiment')}} group by read_ts, ticker
 
-select * from {{ref('stg_sentiment')}} a join {{ref('book_value')}} b 
-
+select b.ts, b.ticker, avg(a.sentiment.compound) sentiment  from {{ref('stg_sentiment')}} a join {{ref('book_value')}} b 
 on a.read_ts = b.ts
+and a.ticker = b.ticker
+group by b.ts, b.ticker
 
 --select distinct read_ts from {{ref('stg_sentiment')}}

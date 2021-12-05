d4693fea94a36815ce614eef8ab1c88954a38a2f
Ivan Trusov <prasad.kona@databricks.com>
2021-12-05 17:35:54 +0000

    df

--- models/market_intel.sql
+++ models/market_intel.sql
@@ -1,7 +1,8 @@
-
-select b.ts, b.ticker, avg(a.sentiment.compound) sentiment  from {{ref('stg_sentiment')}} a join {{ref('book_value')}} b 
+with latest as (select last(shares) over (partition by ticker order by ts desc) last_s, last(value) over (partition by ticker order by ts desc) last_v from {{ref('book_value')}})
+select b.ts, b.ticker, last_v value, last_s shares, avg(a.sentiment.compound) sentiment  
+from {{ref('stg_sentiment')}} a join 
+{{ref('book_value')}} b 
 on a.read_ts = b.ts
 and a.ticker = b.ticker
-group by b.ts, b.ticker
-
---select distinct read_ts from {{ref('stg_sentiment')}}
+join latest
+group by b.ts, b.ticker, last_v, last_s 

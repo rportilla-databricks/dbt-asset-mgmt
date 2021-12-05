cfeecf97530cacd60f67abcca7624948efccc492
Ivan Trusov <prasad.kona@databricks.com>
2021-12-05 19:22:44 +0000

    s

--- models/market_intel.sql
+++ models/market_intel.sql
@@ -1,7 +1,8 @@
 with latest as (
     select 
     {% for latest_metric in ["shares", "value"] %}
-    last({{latest_metric}}) over (partition by ticker order by ts desc) last_{{latest_metric}},
+    last({{latest_metric}}) over (partition by ticker order by ts desc) last_{{latest_metric}}
+    {% if not loop.last %}, {% endif %}
     {% endfor %} 
     from {{ref('book_value')}})
 select b.ts, b.ticker, last_value value, last_shares shares, avg(a.sentiment.compound) sentiment  
@@ -10,4 +11,4 @@ from {{ref('stg_sentiment')}} a join
 on a.read_ts = b.ts
 and a.ticker = b.ticker
 join latest
-group by b.ts, b.ticker, last_v, last_s 
+group by b.ts, b.ticker, last_value, last_shares

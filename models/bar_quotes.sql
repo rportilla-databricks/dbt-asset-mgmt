1005ef1faa08405daa58747b063e52389257b095
Ivan Trusov <prasad.kona@databricks.com>
2021-12-05 17:16:23 +0000

    ss

--- models/bar_quotes.sql
+++ models/bar_quotes.sql
@@ -8,7 +8,7 @@
 select date, concat(ticker, ts) tt_key, ts, ticker, avg_bid_pr, avg_ask_pr, avg_mid_pr 
   from (
 select date,
-    cast(concat(date(ts), ' ', lpad(hour(ts), 2, '0'), ':', lpad(minute(ts), 2, '0'),  ':', '00') as timestamp) ts,
+    cast(concat(date(ts), ' ', lpad(hour(ts), 2, '0'), ':', '00',  ':', '00') as timestamp) ts,
     ticker,
     avg(bid_pr) avg_bid_pr,
     avg(ask_pr) avg_ask_pr, 
